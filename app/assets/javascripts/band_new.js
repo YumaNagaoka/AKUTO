87cbd8ae4bc72d24661d2d164eedc9e6b28b35d0
Nagaoka <yuma.nagaoka@nadai.jp>
2019-04-08 21:15:59 +0900

    Update jquery

--- app/assets/javascripts/band_new.js
+++ app/assets/javascripts/band_new.js
@@ -1,9 +1,11 @@
 $(function(){
     $("#number").change(function(){
         var idNo = $("#number").val();
+        var str = new String();
         for(var i = 1; i <= idNo; i++){
-            $(".add-member")
-            .append("<input type=\"text\" name=\"member[]\" id=\"member[]\">");//メンバーフォームを追加する
+            str += "<input type=\"text\" name=\"member[]\" id=\"member[]\"><br>";
         }
+        $(".add-member")
+            .html(str);//メンバーフォームを追加する
     });
 });
